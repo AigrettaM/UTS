16789399d6173771c10ba60004cd12d53ca537c0
Moreno Heber Fresa <morenobrahmana@gmail.com>
2025-03-25 19:33:18 +0700

    Fix Register.js

--- js/register.js
+++ js/register.js
@@ -1,15 +1,17 @@
 function handleRegister(event) {
     event.preventDefault();
 
-    let nama = document.getElementById("nama").value;
-    let nik = document.getElementById("nik").value;
+    let nama = document.getElementById("nama").value.trim();
+    let nik = document.getElementById("nik").value.trim();
     let golonganDarah = document.getElementById("golongan-darah").value;
-    let gender = document.querySelector('input[name="gender"]:checked');
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
+    let genderElement = document.querySelector('input[name="gender"]:checked');
+    let username = document.getElementById("username").value.trim();
+    let password = document.getElementById("password").value.trim();
+
+    let gender = genderElement ? genderElement.value : "";
 
     if (nama && nik && golonganDarah && gender && username && password) {
-        alert(`Pendaftaran berhasil!\nNama: ${nama}\nUsername: ${username}\nNIK: ${nik}\nGolongan Darah: ${golonganDarah}\nJenis Kelamin: ${gender.value}`);
+        alert(`Pendaftaran berhasil!\nNama: ${nama}\nUsername: ${username}\nNIK: ${nik}\nGolongan Darah: ${golonganDarah}\nJenis Kelamin: ${gender}`);
         window.location.href = "login.html";
     } else {
         alert("Harap isi semua kolom!");
