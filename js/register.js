3911ab9d158b3f4831aec0cc47574f524cc70a30
Moreno Heber Fresa <morenobrahmana@gmail.com>
2025-03-25 19:22:26 +0700

    Fix Register.js

--- js/register.js
+++ js/register.js
@@ -5,10 +5,11 @@ function handleRegister(event) {
     let nik = document.getElementById("nik").value;
     let golonganDarah = document.getElementById("golongan-darah").value;
     let gender = document.querySelector('input[name="gender"]:checked');
+    let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    if (nama && nik && golonganDarah && gender && password) {
-        alert(`Pendaftaran berhasil!\nNama: ${nama}\nNIK: ${nik}\nGolongan Darah: ${golonganDarah}\nJenis Kelamin: ${gender.value}`);
+    if (nama && nik && golonganDarah && gender && usernama && password) {
+        alert(`Pendaftaran berhasil!\nNama: ${nama}\nUsername: ${username}\nNIK: ${nik}\nGolongan Darah: ${golonganDarah}\nJenis Kelamin: ${gender.value}`);
         window.location.href = "login.html";
     } else {
         alert("Harap isi semua kolom!");
